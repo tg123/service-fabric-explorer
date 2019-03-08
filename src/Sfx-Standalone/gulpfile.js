922360175c46dc934ff91d5f1a8348709d98c7fe
Steven Shan <stevshan@microsoft.com>
2019-03-08 13:53:37 -0800

    save

--- src/Sfx-Standalone/gulpfile.js
+++ src/Sfx-Standalone/gulpfile.js
@@ -4,5 +4,30 @@
 //-----------------------------------------------------------------------------
 
 const gulp = require("gulp");
+const cookie = require("cookie.gulp");
+const dd = require("cookie.gulp/dynamic-dependency");
+const { Transform, PassThrough } = require("stream");
+
+/**
+ * 
+ * @param {IElectronLinuxInstallerProcessorConfig} config 
+ * @param {IBuildTaget} buildTarget 
+ * @param {IBuildInfos} buildInfos 
+ * @param {IPackageConfig} packageJson 
+ * @returns {NodeJS.ReadWriteStream}
+ */
+function constructProcessor(config, buildTarget, buildInfos, packageJson) {
+    return new Transform({
+        objectMode: true,
+         /** @param {import("vinyl")} chunk */
+         transform(chunk, encoding, callback) {
+            console.log("Is electron-installer-debian Installed: ", dd.isModuleInstalled("electron-installer-debian"));
+            this.push(chunk);
+            callback();
+        }
+    });
+}
+
+cookie.processor("test-p", constructProcessor);
 
 require("cookie.gulp")(gulp.registry());
