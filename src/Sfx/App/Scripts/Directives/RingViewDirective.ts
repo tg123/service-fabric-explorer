70a51aa5454cf509ee992566c31260b0cd954775
Boshi LIAN <bolian@microsoft.com>
2019-10-30 17:28:51 -0700

    add ws auto recreate

--- src/Sfx/App/Scripts/Directives/RingViewDirective.ts
+++ src/Sfx/App/Scripts/Directives/RingViewDirective.ts
@@ -7,69 +7,93 @@ module Sfx {
     public replace = true;
     public templateUrl = "partials/ring-view.html";
     public scope = {
-        nodes: "="
+        nodes: "=",
     };
 
+    
+
+    // constructor(){
+    //   'ngInject';
+    //   console.log($scope);
+    // }
+
+
     public link($scope: any, element: JQuery, attributes: any, ctrl: DetailViewPartController) {
+
       $scope.nodesData = [];
       $scope.statesData = [];
-      $scope.nodes.collection.forEach((n) => {
-        $scope.nodesData.push(n.raw);
-        $scope.statesData.push({nodeId: n.raw.Id.Id});
-      });
+      // $scope.nodes.collection.forEach((n) => {
+      //   $scope.nodesData.push(n.raw);
+      //   $scope.statesData.push({nodeId: n.raw.Id.Id});
+      // });
+      // $scope.myDataset = [100, 200, 300, 400, 500];
       
       var ws:WebSocket;
-      if (true) {
-          ws = new WebSocket('ws://127.0.0.1:10546');
 
-          ws.onopen = function () {
-            var port = 22980;
-            $scope.nodes.collection.forEach((n) => {
-              //console.log(n.raw.IpAddressOrFQDN);
-              ws.send(JSON.stringify({address: "127.0.0.1:" + (port++), messageType: "init"}));
 
-            });
-          };
+      let recreateWs = function() {
+        // TODO load from config
+        ws = new WebSocket('ws://127.0.0.1:10286/debug');
+  
+        ws.onclose = function (event) {
+          console.log(event)
+          recreateWs();
+        };
+  
+        ws.onerror = function (error) {
+          console.log(error)
+          recreateWs();
+        };
+        
+        ws.onmessage = function (message) {
+          var json;
+          try {
+            json = JSON.parse(message.data);
+          } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
+          }
+          $scope.messageHandler(json);
+        };
       }
-      else {
-          var ws = new WebSocket('ws://' + window.location.hostname + ':' + 22980);
-          ws.onopen = function () {
-            var port = 22980;
-            $scope.nodes.collection.forEach((n) => {
-              ws.send(JSON.stringify({address: "127.0.0.1:" + (port++), messageType: "init"}));
-            });
-          };
-      }
 
-      ws.onclose = function (event) {
-        console.log(event)
-      };
+      recreateWs();
 
-      ws.onerror = function (error) {
-        console.log(error)
-      };
+      // if (true) {
+      //     // ws = new WebSocket('ws://127.0.0.1:10546');
+      //     // ws = new WebSocket('ws://10.31.62.28/debug');
+          
 
-      ws.onmessage = function (message) {
-        var json;
-        try {
-          json = JSON.parse(message.data);
-        } catch (e) {
-          console.log('This doesn\'t look like a valid JSON: ', message.data);
-          return;
-        }
-        $scope.messageHandler(json);
-      };
+      //     // ws.onopen = function () {
+      //     //   var port = 10546;
+      //     //   $scope.nodes.collection.forEach((n) => {
+      //     //     //console.log(n.raw.IpAddressOrFQDN);
+      //     //     ws.send(JSON.stringify({address: "172.17.27.113:" + (port++), messageType: "init"}));
+
+      //     //   });
+      //     // };
+      // }
+      // else {
+      //     var ws = new WebSocket('ws://' + window.location.hostname + ':' + 22980);
+      //     ws.onopen = function () {
+      //       var port = 22980;
+      //       $scope.nodes.collection.forEach((n) => {
+      //         ws.send(JSON.stringify({address: "127.0.0.1:" + (port++), messageType: "init"}));
+      //       });
+      //     };
+      // }
+
 
 
       $scope.messageHandler = function (node: any) {
-        $scope.preprocess(node);
         console.log(node)
-        var index = _.findIndex($scope.statesData, function (o: any) { return o.nodeId == node.nodeId; })
+        $scope.preprocess(node);
+        var index = _.findIndex($scope.statesData, function (o: any) { return o.node_id == node.node_id; })
         if (index >= 0) {
           $scope.statesData[index] = node;
         }
         else {
-          var insertIndex = _.sortedIndexBy($scope.statesData, node, function (o: any) { return o.nodeId; });
+          var insertIndex = _.sortedIndexBy($scope.statesData, node, function (o: any) { return o.node_id; });
           $scope.statesData.splice(insertIndex, 0, node);
 
           //$scope.statesData.push(node);
@@ -79,9 +103,9 @@ module Sfx {
       }
 
       $scope.preprocess = function (node: any) {
-        node.nodeId = node.nodeId.padStart(32, "0");
-        node.routingTokenStart = node.routingTokenStart.padStart(32, "0");
-        node.routingTokenEnd = node.routingTokenEnd.padStart(32, "0");
+        node.node_id = node.node_id.padStart(32, "0");
+        node.routing_token_start = node.routing_token_start.padStart(32, "0");
+        node.routing_token_end = node.routing_token_end.padStart(32, "0");
       }
     }
   }
