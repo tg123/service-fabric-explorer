eb4771d53dcd1f7f2e49d1cc165c26a11044a151
Boshi LIAN <bolian@microsoft.com>
2020-03-23 08:02:18 -0700

    gw

--- src/Sfx/App/Scripts/Services/HttpService.ts
+++ src/Sfx/App/Scripts/Services/HttpService.ts
@@ -35,23 +35,68 @@ module Sfx {
             this.$http.defaults.headers.common[Constants.SfxTelemetryMetadataName] = Constants.SfxTelemetryHeaderValue;
         }
 
+        insertParam(url:string, key: string, value: string): string
+        {
+            key = encodeURI(key); value = encodeURI(value);
+
+            var kvp = url.split('&');
+
+            var i=kvp.length; var x; while(i--) 
+            {
+                x = kvp[i].split('=');
+
+                if (x[0]==key)
+                {
+                    x[1] = value;
+                    kvp[i] = x.join('=');
+                    break;
+                }
+            }
+
+            if(i<0) {kvp[kvp.length] = [key,value].join('=');}
+
+            //this will reload the page, it's likely better to store this until finished
+            return kvp.join('&'); 
+        }
+
+        
+        getapp(): string
+        {
+
+            // var url = new URL(window.location.href);
+            // return url.searchParams.get("serviceapp");
+            return new URLSearchParams(window.location.hash.substring(1).split('?')[1]).get('serviceapp')
+        }
+
         public getAsync<T>(url: string): angular.IHttpPromise<T> {
+
+            url = this.insertParam(url, "serviceapp", this.getapp())
+            // console.log(url);
+
             return this.httpClient ? this.requestAsync({ method: "GET", url: url }) : this.$http.get(url);
         }
 
         public postAsync<T>(url: string, data: any): angular.IHttpPromise<T> {
+            url = this.insertParam(url, "serviceapp", this.getapp())
+            // console.log(url);
             return this.httpClient ? this.requestAsync({ method: "POST", url: url, body: data }) : this.$http.post(url, data);
         }
 
         public putAsync<T>(url: string, data: any): angular.IHttpPromise<T> {
+            url = this.insertParam(url, "serviceapp", this.getapp())
+            // console.log(url);
             return this.httpClient ? this.requestAsync({ method: "PUT", url: url, body: data }) : this.$http.put(url, data);
         }
 
         public patchAsync<T>(url: string, data: any): angular.IHttpPromise<T> {
+            url = this.insertParam(url, "serviceapp", this.getapp())
+            // console.log(url);
             return this.httpClient ? this.requestAsync({ method: "PATCH", url: url, body: data }) : this.$http.patch(url, data);
         }
 
         public deleteAsync<T>(url: string): angular.IHttpPromise<T> {
+            url = this.insertParam(url, "serviceapp", this.getapp())
+            // console.log(url);
             return this.httpClient ? this.requestAsync({ method: "DELETE", url: url }) : this.$http.delete(url);
         }
 
